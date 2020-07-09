7cc34cbd00e77b1d48ffa224e2829d60ed203ca3
Jimmy_Jiang <jimmyjiang914@gmail.com>
2020-07-09 01:11:51 -0400

    start script

---
+++ redacted-understood-eda/understood.R
@@ -0,0 +1,12 @@
+library(skimr)
+library(WVPlots)
+library(plotly)
+library(class)
+library(MLmetrics)
+
+#Goal: We want to see whether income and/or journey state can be used to predict question response
+
+data <- read.csv("") # Import data
+data
+
+summary(data)
